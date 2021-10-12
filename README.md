63e1f45c9f478520c1c4f8829bc4c2c653e4cb5e
Mizan Muhammad Toyyibun <88652499+mizantoyyibun@users.noreply.github.com>
2021-10-12 11:58:05 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # indomarinefish
-This repository will be a draft of a package about the latest total marine fish production in Indonesia.  Data will be collected from PIPP (Pusat Informasi Pelabuhan Perikanan).
+This package will give us information about the latest total marine fish production in Indonesia. 
+The Name of the fish is written in Indonesian. The total production is in Kg.
+
+## How it work?
+This package will scrape from [PIPP](http://pipp.djpt.kkp.go.id/produksi_harga) 
+to get the latest marine fish production in Indonesia.
+
+This package will use BeautifulSoup4 and Request, to produce output in the form of JSON. 
+This package is ready to be used in web or mobile applications.
+
+## How to use
+    import produksiikan
+
+    if __name__ == '__main__':
+        print('Aplikasi Utama')
+        result = produksiikan.data_extraction()
+        produksiikan.show_data(result)
+        
+# Author
+mizan toyyibun
---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+https://packaging.python.org/tutorials/packaging-projects/
+https://www.markdownguide.org/cheat-sheet/
+"""
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="indonesian-fishproduction",
+    version="0.1",
+    author="mizan toyyibun",
+    author_email="untirta.mizan62@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wearenotrobot/indomarinefish",
+    project_urls={
+        "Bug Tracker": "https://github.com/wearenotrobot/indomarinefish",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    # package_dir={"": "src"},
+    # packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
